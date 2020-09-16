e86f03f13654e480d94b86c8f8c958eeccacf069
nulLeeKH <i_am@nulleekh.com>
2020-09-17 08:49:53 +0900

    [FEATURE] Implement run_situation function

--- monty_hall_problem.py
+++ monty_hall_problem.py
@@ -3,10 +3,20 @@
 import random
 
 def run_situation(selected, switch, opened, answer):
-    return
+    if switch:
+        if selected == answer:
+            return False
+        else:
+            return True
+    else:
+        if selected == answer:
+            return True
+        else:
+            return False
 
 def run_simulation():
     return
 
 if __name__ == "__main__":
     result = run_simulation()
+
