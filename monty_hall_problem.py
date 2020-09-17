bdc1534a48c64830a2f10498f49df28d4f7aabfb
nulLeeKH <i_am@nulleekh.com>
2020-09-17 09:37:04 +0900

    [FEATURE] Implement run_simulation function

--- monty_hall_problem.py
+++ monty_hall_problem.py
@@ -26,6 +26,19 @@ def run_simulation(count):
             "lose": 0
         }
     }
+
+    for i in range(count):
+        selected = random.randrange(3)
+        answer = random.randrange(3)
+        if selected == answer:
+            opened = answer
+            while answer == opened:
+                opened = random.randrange(3)
+        else:
+            opened = 3 - (selected + answer)
+        for switch in [True, False]:
+            t = run_situation(selected, answer, opened, switch)
+            result["switch" if switch else "unswitch"]["win" if t else "lose"] += 1
     
     return result
 
@@ -35,4 +48,5 @@ if __name__ == "__main__":
         exit()
     count = int(sys.argv[1])
     result = run_simulation(count)
+    print(result)
 
